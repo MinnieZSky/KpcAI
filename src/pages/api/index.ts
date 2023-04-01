5fcb3ec5249e79a18a2b91ee50541afff9c6958d
Dennis <zhwffy@hotmail.com>
2023-04-01 13:54:34 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -36,7 +36,7 @@ export const config = {
 }
 
 //export const localKey = import.meta.env.OPENAI_API_KEY || ""
-export const localKey = process.env.SENDKEY || ""
+const localKey = process.env.SENDKEY || ""
 export const baseURL = import.meta.env.NOGFW
 //const key= process.env.SENDKEY
 
